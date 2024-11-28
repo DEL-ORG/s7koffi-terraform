0649ad40d04db1fe45045d4bfd3cebf3f7e20759
koffimensah <mensahson74@gmail.com>
2024-11-28 01:08:58 -0500

    creating terraform module and resource

---
+++ module/s3-backend/variable.tf
@@ -0,0 +1,23 @@
+variable "aws_region_main" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "aws_region_backup" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "common_tags" {
+  description = "A map of tags to be applied to all resources"
+  type        = map(string)
+  default = {
+    id             = "2024"
+    owner          = "s7koffimensah"
+    environment    = "dev"
+    project        = "my-project"
+    create_by      = "Terraform"
+    cloud_provider = "aws"
+
+  }
+}
